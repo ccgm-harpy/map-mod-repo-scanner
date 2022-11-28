64d23f682c7af8a4dfdc6ea12c38b54bd7ce09eb
protochime327 <117958419+ccgm-harpy@users.noreply.github.com>
2022-11-28 00:12:30 -0700

    Fixed backslash in path for Linux and more
    Backslashes in my code have been replaced so they don't cause issues with Linux.
    
    Also added exclusions for some files that are added to your maps folder by git.

--- lib/MMRS.py
+++ lib/MMRS.py
@@ -5,26 +5,29 @@ from lib.MMZ import MapModZip
 class MapModRepoScanner():
     def __init__(self, path):
         self.repoPath = path
-        self.mapsPath = f"{self.repoPath}\\maps"
-        self.indexFilePath = f"{self.repoPath}\\indexfile"
+        self.mapsPath = f"{self.repoPath}/maps"
+        self.indexFilePath = f"{self.repoPath}/indexfile"
+        self.zipScanExclusions = [".gitkeep"]
 
     def valid_map_list(self):
-        files = listdir(self.mapsPath)
+        files = [m for m in listdir(self.mapsPath) if not m in self.zipScanExclusions]
 
         for file in files:
             if isdir(file):
-                print(f"Found extra directory in your maps folder '{self.mapsPath}\\{file}'")
+                print(f"Found extra directory in your maps folder '{self.mapsPath}/{file}'")
                 return False
 
             if not file.endswith(".zip"):
-                print(f"File in maps folder that's not a zip '{self.mapsPath}\\{file}'")
+                print(f"File in maps folder that's not a zip '{self.mapsPath}/{file}'")
                 return False
 
         return True
 
     def map_list(self):
         mapList = listdir(self.mapsPath)
-        return [map.replace(".zip", "") for map in mapList]
+        return [
+            m.replace(".zip", "") for m in mapList if not m in self.zipScanExclusions
+        ]
 
     def read_index_file(self):
         with open(self.indexFilePath, "r") as f:
@@ -73,10 +76,10 @@ class MapModRepoScanner():
         mapList = listdir(self.mapsPath)
 
         for _mapZip in mapList:
-            mapZip = MapModZip(f"{self.mapsPath}\\{_mapZip}")
+            mapZip = MapModZip(f"{self.mapsPath}/{_mapZip}")
 
             if not mapZip.has_config():
-                print(f"The map {self.mapsPath}\\{_mapZip} is missing a map.config file")
+                print(f"The map {self.mapsPath}/{_mapZip} is missing a map.config file")
                 return False
             
             if not mapZip.has_map_files():
@@ -85,11 +88,11 @@ class MapModRepoScanner():
             configStr = mapZip.read_config()
 
             if not mapZip.valid_config_modes(configStr):
-                print(f"The map {self.mapsPath}\\{_mapZip} has invalid map.config modes setting")
+                print(f"The map {self.mapsPath}/{_mapZip} has invalid map.config modes setting")
                 return False
 
             if not mapZip.valid_config_size(configStr):
-                print(f"The map {self.mapsPath}\\{_mapZip} has invalid map.config size setting")
+                print(f"The map {self.mapsPath}/{_mapZip} has invalid map.config size setting")
                 return False
 
         return True
